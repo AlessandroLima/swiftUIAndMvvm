bd80b55a6fe1e4d1943ce2bfeaff3e41e1240820
Alessandro Teixeira Lima <alessandro.lima-terceiro@cea.com.br>
2022-05-20 19:56:56 -0300

    Class 1

--- swiftMVVM/swiftMVVM/ContentView.swift
+++ swiftMVVM/swiftMVVM/ContentView.swift
@@ -8,14 +8,69 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject private var model: ContentViewModel
+    
+    init(model: ContentViewModel){
+        self.model = model
+    }
+
     var body: some View {
-        Text("Hello, world!")
+        Text(model.state.isLoading ? "Carregando..." : model.state.message)
+            .onAppear(perform: self.model.loadData)
             .padding()
     }
+    
+    private var loadingOverlay: some View {
+        Group {
+            if model.state.isLoading {
+                Text("Carregando...")
+            }
+        }
+    }
+    
+    
+}
+
+struct ContentViewState {
+    var message = ""
+    var isLoading = false
+
+}
+
+class ContentViewModel: ObservableObject {
+//    private(set) var state: ContentViewState {
+//        didSet {
+//            objectWillChange.send()
+//        }
+//    }
+
+    // A implementação abaixo funciona como a dew cima
+    
+    @Published  private(set) var state: ContentViewState
+    
+    init(initialState: ContentViewState = .init()) {
+        state = initialState
+    }
+    
+    func loadData() {
+        state.isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(5)){ [self] in
+            self.state.isLoading = false
+            self.state.message = "Acabei de carregar..."
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group{
+            ContentView(model: .init(initialState: .init(isLoading: true)))
+                .previewDisplayName("Loading")
+            ContentView(model: .init(initialState: .init(message: "Acabei de carregar!", isLoading: false)))
+                .previewDisplayName("Loaded")
+        }
+        
     }
 }
--- swiftMVVM/swiftMVVM/swiftMVVMApp.swift
+++ swiftMVVM/swiftMVVM/swiftMVVMApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct swiftMVVMApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(model: .init())
         }
     }
 }
