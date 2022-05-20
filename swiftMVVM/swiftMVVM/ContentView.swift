01bd9b3238c4cd406953a65a8a634ba496ef98b4
Alessandro Teixeira Lima <alessandro.lima-terceiro@cea.com.br>
2022-05-20 18:36:27 -0300

    Inicial

---
+++ swiftMVVM/swiftMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftMVVM
+//
+//  Created by Alessandro Teixeira Lima on 20/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftMVVM/swiftMVVM/swiftMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftMVVMApp.swift
+//  swiftMVVM
+//
+//  Created by Alessandro Teixeira Lima on 20/05/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
